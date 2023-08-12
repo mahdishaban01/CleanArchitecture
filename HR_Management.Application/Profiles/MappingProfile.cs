7e8dd8822e422d8e4716113c3cb0b05603472f44
mahdishaban01 <mahdishaban33@gmail.com>
2023-08-12 19:19:30 +0330

    MappingProfile

--- HR_Management.Application/Profiles/MappingProfile.cs
+++ HR_Management.Application/Profiles/MappingProfile.cs
@@ -4,9 +4,29 @@
     {
         public MappingProfile()
         {
+            #region LeaveRequest
+
             CreateMap<LeaveRequest, LeaveRequestDTO>().ReverseMap();
+            CreateMap<LeaveRequest, CreateLeaveRequestDTO>().ReverseMap();
+            CreateMap<LeaveRequest, UpdateLeaveRequestDTO>().ReverseMap();
+
+            #endregion
+
+            #region LeaveAllocation
+
             CreateMap<LeaveAllocation, LeaveAllocationDTO>().ReverseMap();
+            CreateMap<LeaveAllocation, CreateLeaveAllocationDTO>().ReverseMap();
+            CreateMap<LeaveAllocation, UpdateLeaveAllocationDTO>().ReverseMap();
+
+            #endregion
+
+            #region LeaveType
+
             CreateMap<LeaveType, LeaveTypeDTO>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeDTO>().ReverseMap();
+            CreateMap<LeaveType, UpdateLeaveTypeDTO>().ReverseMap();
+
+            #endregion
         }
     }
 }
