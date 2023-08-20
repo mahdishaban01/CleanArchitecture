869e56c23022ccd18ee6a516aa53ba34b8a0ca57
mahdishaban01 <mahdishaban33@gmail.com>
2023-08-20 16:51:53 +0330

    Create Test Project

---
+++ HR_Management.Application.UnitTests/Mocks/MockRepository.cs
@@ -0,0 +1,40 @@
+﻿using HR_Management.Application.Contracts.Persistence;
+using HR_Management.Domain.Entities;
+using Moq;
+
+namespace HR_Management.Application.UnitTests.Mocks
+{
+    public static class MockLeaveRepository
+    {
+        public static Mock<ILeaveTypeRepository> GetLeaveTypeRepository()
+        {
+            var leaveTypes = new List<LeaveType>()
+            {
+                new LeaveType
+                {
+                Id = 1,
+                DefaultDay = 10,
+                Name = "Test Vacation"
+                },
+                new LeaveType
+                {
+                Id = 2,
+                DefaultDay = 15,
+                Name = "Test Sick"
+                }
+            };
+
+            var mockRepo = new Mock<ILeaveTypeRepository>();
+            mockRepo.Setup(r => r.GetAll()).ReturnsAsync(leaveTypes);
+
+            mockRepo.Setup(r => r.Add(It.IsAny<LeaveType>()))
+                .ReturnsAsync((LeaveType leavetype) =>
+                {
+                    leaveTypes.Add(leavetype);
+                    return leavetype;
+                });
+
+            return mockRepo;
+        }
+    }
+}
