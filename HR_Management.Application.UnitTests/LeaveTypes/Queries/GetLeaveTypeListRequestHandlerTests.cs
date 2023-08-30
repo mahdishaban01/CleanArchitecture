fe30e0a872ee6dd7608cff01fd9cab988a18e296
Mehdi Shaban <mahdishaban33@gmail.com>
2023-08-30 16:31:38 +0330

    LeaveType Query Test

---
+++ HR_Management.Application.UnitTests/LeaveTypes/Queries/GetLeaveTypeListRequestHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using HR_Management.Application.Contracts.Persistence;
+using HR_Management.Application.DTOs.LeaveType;
+using HR_Management.Application.Features.LeaveType.Handlers.Queries;
+using HR_Management.Application.Features.LeaveType.Requests.Queries;
+using HR_Management.Application.Profiles;
+using HR_Management.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+
+namespace HR_Management.Application.UnitTests.LeaveTypes.Queries
+{
+    public class GetLeaveTypeListRequestHandlerTests
+    {
+        IMapper _mapper;
+        Mock<ILeaveTypeRepository> _mockRepository;
+        public GetLeaveTypeListRequestHandlerTests()
+        {
+            _mockRepository = MockLeaveRepository.GetLeaveTypeRepository();
+
+            var mapperConfig = new MapperConfiguration(m =>
+            {
+                m.AddProfile<MappingProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetLeaveTypeListTest()
+        {
+            var handler = new GetLeaveTypeListRequestHandler(_mockRepository.Object, _mapper);
+
+            var result = await handler.Handle(new GetLeaveTypeListRequest(), CancellationToken.None);
+
+            result.ShouldBeOfType<List<LeaveTypeDTO>>();
+            result.Count.ShouldBe(2);
+
+        }
+    }
+}
