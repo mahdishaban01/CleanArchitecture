48c8e828be5eacbb068e12b2d603c8ad256d7ce4
Mahdi Shaban <mahdishaban33@gmail.com>
2024-01-12 20:55:41 +0330

    Add Account Controller

---
+++ HR_Management.Api/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using HR_Management.Application.Contracts.Identity;
+using HR_Management.Application.Models.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR_Management.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AccountController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponse>> Login(AuthRequest request)
+        {
+            return Ok(await _authService.Login(request));
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<RegisterationResponse>> Register(RegisterationRequest request)
+        {
+            return Ok(await _authService.Register(request));
+        }
+    }
+}
