ea85805753cc446b501ea3d88d213af448175d8b
Mahdi Shaban <mahdishaban33@gmail.com>
2024-01-13 17:49:39 +0330

    Config Swagger

--- HR_Management.Api/Program.cs
+++ HR_Management.Api/Program.cs
@@ -1,6 +1,7 @@
 using HR_Management.Application;
 using HR_Management.Infrastructure;
 using HR_Management.Persistence;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -43,3 +44,44 @@ app.UseCors("CorsPolicy");
 app.MapControllers();
 
 app.Run();
+
+void AddSwagger(IServiceCollection services)
+{
+    services.AddSwaggerGen(options =>
+    {
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+        {
+            Description = "JWT Authorization header using the Bearer scheme. " +
+            "Enter 'Bearer' [space] and then your token in the text input below." +
+            "Example: 'Bearer 1234sddsw",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+        {
+            {
+            new OpenApiSecurityScheme()
+                {
+                    Reference = new OpenApiReference()
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    },
+                    Scheme = "oauth2",
+                    Name = "Bearer",
+                    In = ParameterLocation.Header
+                },
+                new List<string>()
+            }
+        });
+
+        options.SwaggerDoc("v1", new OpenApiInfo()
+        {
+            Version = "v1",
+            Title = "HR Management Api"
+        });
+    });
+}
