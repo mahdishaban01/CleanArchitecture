0e5233702ee29cb86f8b6d1ebbb71a3151dd9802
Mahdi Shaban <mahdishaban33@gmail.com>
2024-01-17 09:08:39 +0330

    Authenticate Service

---
+++ HR_Management.MVC/Contracts/IAuthenticateService.cs
@@ -0,0 +1,9 @@
+﻿namespace HR_Management.MVC.Contracts
+{
+    public interface IAuthenticateService
+    {
+        Task<bool> Authenticate(string email,string password);
+        Task<bool> Register(string firstName,string lastName,string username,string password);
+        Task Logout(); 
+    }
+}
---
+++ HR_Management.MVC/Services/AuthenticateService.cs
@@ -0,0 +1,22 @@
+﻿using HR_Management.MVC.Contracts;
+
+namespace HR_Management.MVC.Services
+{
+    public class AuthenticateService : IAuthenticateService
+    {
+        public Task<bool> Authenticate(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Register(string firstName, string lastName, string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
