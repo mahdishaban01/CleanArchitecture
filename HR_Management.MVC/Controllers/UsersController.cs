e51edeb508775d8aab6f11365ed4d10328ab4a89
Mahdi Shaban <mahdishaban33@gmail.com>
2024-01-18 23:40:46 +0330

    Login View Action

---
+++ HR_Management.MVC/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using HR_Management.MVC.Contracts;
+using HR_Management.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR_Management.MVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        private IAuthenticateService _authenticateService;
+
+        public UsersController(IAuthenticateService authenticateService)
+        {
+            _authenticateService = authenticateService;
+        }
+
+        #region Login
+
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM login, string returnUrl)
+        {
+            returnUrl ??= Url.Content("~/");
+            var isLoggedIn = await _authenticateService.Authenticate(login.Email, login.Passsword);
+            if (isLoggedIn)
+            {
+                return LocalRedirect(returnUrl);
+            }
+
+            ModelState.AddModelError("","Login Failed. Please Try again.");
+            return View(login);
+        }
+
+        #endregion
+    }
+}
---
+++ HR_Management.MVC/Models/LoginVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HR_Management.MVC.Models
+{
+    public class LoginVM
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Passsword { get; set; }
+        public string? ReturnUrl { get; set; }
+    }
+}
